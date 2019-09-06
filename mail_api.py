98f2c5aa231315ffb7382e5e0e98bb3ed0184512
Pithagoras Leal <39030379+pithagorasleal@users.noreply.github.com>
2019-09-06 11:59:21 -0300

    Update mail_api.py

--- mail_api.py
+++ mail_api.py
@@ -6,3 +6,7 @@ bp_mail = Blueprint('BP mail', __name__)
 @bp_mail.route('/mail_service/promo', methods=['POST'])
 def email_promo():
     request.get_json()
+
+@bp_mail.route('/mail_service/email_sac', methods=['POST'])
+def email_sac():
+    request.get_json()
