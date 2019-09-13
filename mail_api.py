231fa0eb15512b40b5fe07ba995916d6e01445a5
Pithagoras Leal <39030379+pithagorasleal@users.noreply.github.com>
2019-09-13 19:16:20 -0300

    Update mail_api.py

--- mail_api.py
+++ mail_api.py
@@ -5,8 +5,16 @@ bp_mail = Blueprint('BP mail', __name__)
 
 @bp_mail.route('/mail_service/promo', methods=['POST'])
 def email_promo():
-    request.get_json()
+    
+    promo_email = request.get_json()
+    #Chamar camada logica passando promo_email
+    
+    return 'Retorno da Camada Logica'
 
 @bp_mail.route('/mail_service/email_sac', methods=['POST'])
 def email_sac():
-    request.get_json()
+    
+    sac_email = request.get_json()
+    #Chamar camada logica passando sac_email
+    
+    return 'Retorno da Camada Logica'
