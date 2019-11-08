6e2041115a1e965ecf31091b09215207c7a25d1c
TyphonBak <andrade.czr@gmail.com>
2019-11-08 15:27:16 -0300

    Adjust get return

--- app/BLL/categoria_service.py
+++ app/BLL/categoria_service.py
@@ -2,6 +2,6 @@ from app.DAO.db_categoria import listar as listar_db
 
 def listar():
     try:
-        return [categoria.serialize() for categoria in listar_db()]
+        return 200, [categoria.serialize() for categoria in listar_db()]
     except Exception:
         return 503, None
