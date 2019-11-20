482a78b54070d99e7d872a39378f677a4c91f057
TyphonBak <andrade.czr@gmail.com>
2019-11-20 17:07:49 -0300

    adjust Produto View for images

--- app/VIEWS/produto.py
+++ app/VIEWS/produto.py
@@ -17,14 +17,14 @@ def buscar(id):
 def criar():
     if not request.json:
         return jsonify(), 400
-    code, conteudo = criar_service(request.json)
+    code, conteudo = criar_service(**request.json)
     return jsonify(conteudo), code
 
 @bp_produto.route('/api/produtos/<int:id>', methods=['PUT'])
 def alterar(id):
     if not request.json:
         return jsonify(), 400
-    code, conteudo = alterar_service(id, request.json)
+    code, conteudo = alterar_service(id, **request.json)
     return jsonify(conteudo), code
 
 @bp_produto.route('/api/produtos/<int:id>', methods=['DELETE'])
